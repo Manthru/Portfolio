a4b41c6476c97208bf0183081b78b08f2eab858a
manthru <manthrunaik455@gmail.com>
2023-09-08 06:13:23 +0530

    added the main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+ src="https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js"
+
+ var typed = new Typed('#element', {
+        strings: ['Web Developer', 'game Designer', 'Softwer Developer', 'Entrepreneur'],
+        typeSpeed: 50,
+    });
